49847f7ebded0ea270116ce1f3149da2cf3bbc20
AleksPilot <aleks.pilot2002@yandex.ru>
2021-12-09 11:05:52 +0700

    Appearance fixed, beta version is available

--- Project Game/main.cpp
+++ Project Game/main.cpp
@@ -12,22 +12,6 @@ using namespace std;
 
 
 int main() {
-    //number a, b;
-    //a.reset();
-    //b.reset();
-    //a.generate();
-    //a.print();
-    //numValue temp;
-    //b = b.convert(1570);
-    //b.print();
-    //a = a.convert(1511);
-    //temp.init();
-    //number c;
-    //cout << c.easy_search(a, 1, 1) << '\n';
-    //temp = temp.checkout(a, b);
-    //temp.print();*/
-    //numValue check;
-    //check.checkout(b, a).print();
     start();
 
     return 0;
